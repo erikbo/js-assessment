dc97fff7fcdb78d783f2e0440051436d81d20678
Erik Bohlin <erik87@gmail.com>
2015-12-15 12:31:58 +0100

    Finished best practices

--- app/async.js
+++ app/async.js
@@ -2,7 +2,10 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
   async : function(value) {
-
+    setTimeout(function() {
+      $.Deferred().resolve(value);
+    }, 10);
+    return dfd.promise();
   },
 
   manipulateRemoteData : function(url) {
